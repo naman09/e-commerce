e10d78b15866b4e4f028f7215cf9fe9e730d2ad3
Naman Jain <n910.jain@gmail.com>
2026-03-16 00:16:25 +0530

    fix:(HomePage)top-navbar size fix, offering cards hoverable

--- frontend/app/components/HomePage/Offerings/OfferingCard.tsx
+++ frontend/app/components/HomePage/Offerings/OfferingCard.tsx
@@ -6,11 +6,11 @@ export interface OfferingCardProps {
 }
 
 export const OfferingCard = (props: OfferingCardProps) => {
-    return <div className="w-100">
-        <img src={props.imageUrl} alt={props.imageAlt} className="border border-gray-100 shadow-md rounded-lg h-100 object-contain"/>
+    return <div className="w-100 group cursor-pointer">
+        <img src={props.imageUrl} alt={props.imageAlt} className="border border-gray-100 transition duration-300 group-hover:shadow-xl group-hover:-translate-y-2 group-hover:scale-105 shadow-md rounded-lg h-100 object-contain"/>
         <div className="mt-4">
-            <p className="font-medium text-center">{props.title}</p>
-            <p className="text-gray-500 text-center">{props.description}</p>
+            <p className="font-medium text-center group-hover:text-black text-lg">{props.title}</p>
+            <p className="text-gray-500 text-center group-hover:text-black">{props.description}</p>
         </div>
     </div>
 }
--- frontend/app/components/TopNavBar.tsx
+++ frontend/app/components/TopNavBar.tsx
@@ -4,7 +4,7 @@ export const TopNavBar = () => {
 
     const navBarItems = ["Marketplace", "About Us", "User"];
 
-    return <div className="flex justify-between sticky top-0 bg-gray-900 text-white z-10 px-10 py-5">
+    return <div className="flex justify-between sticky top-0 bg-gray-900 text-white z-10 px-10 py-2">
         <div className="flex">
             <img src="app/assets/images/blue-whale-icon.png" alt="blue whale corp navbar logo" className="h-15"/>
             <h1 className="text-3xl py-3">&nbsp;BWC</h1>
